a36954f451b8f380eef4f314a082515c41c8ca92
Chad Heuschober <chad.heuschober@cuny.edu>
2015-04-17 22:39:59 -0400

    Better test coverage for task 4.

--- tests/test_task_04.py
+++ tests/test_task_04.py
@@ -15,6 +15,18 @@ class Task04TestCase(unittest.TestCase):
     """Task 04 tests"""
 
 
+    def test_default_tires(self):
+        """Tests that tires are created by default."""
+        mycar = task_04.CustomCar('BabyBlue')
+        self.assertEqual(len(mycar.tires), 4)
+        for tire in mycar.tires:
+            self.assertIsInstance(tire, task_04.CustomTire)
+
+    def test_color_attr(self):
+        """Tests that the color argument of the parent class is respected."""
+        mycar = task_04.CustomCar('BabyBlue')
+        self.assertEqual(mycar.color, 'BabyBlue')
+
     def test_tires_attr(self):
         """Tests that the tires argument exists and sets the attr."""
         tire = task_04.CustomTire(20)
